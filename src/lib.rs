9fca03c7d5ee1041215d8ef367b25ea216870dcf
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-05-12 12:20:01 -0500

    make client for requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+extern crate reqwest;
+extern crate serde;
+use std::collections::HashMap;
+use reqwest::{Client,get};
+use serde::{Deserialize, Serialize};
+pub fn makeclient() -> Result<Client,reqwest::Error> {
+    static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"),"/",env!("CARGO_PKG_VERSION"),);
+    let client = Client::builder().user_agent(APP_USER_AGENT).build();
+    client}
