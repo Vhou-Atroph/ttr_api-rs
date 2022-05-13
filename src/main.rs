a877923d224e1afacea5e7095b6fb5f77e535868
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-05-13 00:13:18 -0500

    Update Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use ttr_api_rs::Population;
-use ttr_api_rs::SillyMeter;
-use ttr_api_rs::Invasions;
-use ttr_api_rs::Offices;
+use ttr_api::Population;
+use ttr_api::SillyMeter;
+use ttr_api::Invasions;
+use ttr_api::Offices;
 fn main() {
-    let pop = Population::PopAPI::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    let pop = Population::PopAPI::new(ttr_api::makeclient().unwrap()).unwrap();
     println!("{:?}",pop);
-    let met = SillyMeter::Meter::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    let met = SillyMeter::Meter::new(ttr_api::makeclient().unwrap()).unwrap();
     println!("{:?}",met);
-    let inv = Invasions::Invasion::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    let inv = Invasions::Invasion::new(ttr_api::makeclient().unwrap()).unwrap();
     println!("{:?}",inv);
-    let off = Offices::Office::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    let off = Offices::Office::new(ttr_api::makeclient().unwrap()).unwrap();
     println!("{:?}",off);
 }
