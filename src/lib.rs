84309ee07514cf6a744c7bff64d0d7565d80ab8b
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-05-12 23:14:56 -0500

    Invasions
    Adding invasions API

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ pub mod Population {
     use reqwest::Client;
     use serde::Deserialize;
     use chrono::prelude::*;
-    use super::*;
     
     ///Struct for the Population API for Toontown Rewritten. See information regarding the API at <https://github.com/ToontownRewritten/api-doc/blob/master/population.md>
     
@@ -111,9 +110,52 @@ pub mod SillyMeter {
 
     impl Meter {
 
+        ///Grabs information from the Silly Meter API and converts it to the Meter struct.
+
         #[tokio::main]
         pub async fn new(client:Client) -> Result<Self,Box<dyn std::error::Error>> {
             let resp =  client.get("https://www.toontownrewritten.com/api/sillymeter").send().await?
+            .json::<Self>()
+            .await?;
+            Ok(resp)}
+    }
+}
+
+///Tools for Toontown Rewritten's Invasions API
+
+pub mod Invasions {
+    extern crate reqwest;
+    extern crate serde;
+    extern crate serde_json;
+    use std::collections::HashMap;
+    use reqwest::Client;
+    use serde::Deserialize;
+
+    ///Struct for the Invasions API for Toontown Rewritten. See information regarding the API at <https://github.com/ToontownRewritten/api-doc/blob/master/invasions.md>
+
+    #[derive(Deserialize,Debug)]
+    pub struct Invasion {
+        pub error: Option<String>,
+        pub invasions: HashMap<String,DistrictInv>,
+        pub lastUpdated: i64,
+    }
+
+    ///Struct for each individual district's invasions
+
+    #[derive(Deserialize,Debug)]
+    pub struct DistrictInv {
+        pub asOf: i64,
+        pub r#type: String,
+        pub progress: String,
+    }
+
+    impl Invasion {
+        
+        ///Grabs information from the Invasions API and converts it to the PopAPI struct.
+
+        #[tokio::main]
+        pub async fn new(client:Client) -> Result<Self,Box<dyn std::error::Error>> {
+            let resp =  client.get("https://www.toontownrewritten.com/api/invasions").send().await?
             .json::<Self>()
             .await?;
             Ok(resp)}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use ttr_api_rs::Population;
 use ttr_api_rs::SillyMeter;
+use ttr_api_rs::Invasions;
 fn main() {
+    let pop = Population::PopAPI::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    println!("{:?}",pop);
+    let met = SillyMeter::Meter::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    println!("{:?}",met);
+    let inv = Invasions::Invasion::new(ttr_api_rs::makeclient().unwrap()).unwrap();
+    println!("{:?}",inv);
 }
