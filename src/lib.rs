167f5ab7d1573ac589d2dfe9aa1d46cbd9df366a
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-04 15:44:54 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-//!A library specialized for contacting Toontown Rewritten APIs. See an example usage of this crate at <https://github.com/Vhou-Atroph/ttr_pop_webserver>.
+//! A library specialized for contacting Toontown Rewritten APIs. See an example usage of this crate at <https://github.com/Vhou-Atroph/ttr_pop_webserver>.
 #![allow(non_snake_case)]
 #![allow(unused_variables)]
 #![deny(clippy::all)]
 extern crate reqwest;
 use reqwest::Client;
 
-///Makes the default client for the API checker.
-
+/// Makes the default client for the API checker. Clients are required to access the API, and this one is provided in case one does not wish to make their own.
 pub fn makeclient() -> Client {
     static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"),"/",env!("CARGO_PKG_VERSION"),);
     Client::builder().user_agent(APP_USER_AGENT).build().unwrap()
